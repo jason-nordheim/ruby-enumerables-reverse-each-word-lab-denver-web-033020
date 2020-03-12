d67febd03d769eb9a6a2f92e920343166aeb7b69
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-12 17:33:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,21 @@
 def reverse_each_word(sentence)
   words = sentence.split(' ')
   r_words = words.each | word | do
-    new_word = []
-    i = word.length
-    while i > 0 do
-      new_word << word[i]
-      i -= 1
-    end
-    return new_word
+
   end
   p r_words
 end
 
 
 def reverse_letter(word)
-
-end 
+  new_word = []
+  i = word.length
+  while i > 0 do
+    new_word << word[i]
+    i -= 1
+  end
+  return new_word
+end
 
 example = "Hello there, and how are you?"
 reverse_each_word(example)
