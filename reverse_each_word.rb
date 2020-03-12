6767f13fd5838d0cd3071a9a061654b7c219cf89
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-12 17:31:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
 def reverse_each_word(sentence)
   words = sentence.split(' ')
-  r_words = words.each { |w| w.reverse }
+  r_words = words.each | word | do
+    new_word = []
+    i = 0
+    while i < word.length do
+
+    end
+  end
   p r_words
 end
 
